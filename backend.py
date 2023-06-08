b893575b404511698b16094ba7be863e6ff9e86f
Xinan <youkingan11@gmail.com>
2023-06-08 18:32:38 +0100

    add st.secrets["API_KEY"]

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
+import streamlit as st
 import requests
 
+API_KEY = st.secrets["API_KEY"]
 def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
